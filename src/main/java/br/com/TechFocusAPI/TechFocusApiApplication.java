5377fead946f8ed9ed7477dbfbe43cb72865fc01
Marco Castellani <marcocastellanictt@gmail.com>
2023-04-28 15:37:28 -0300

    BaseProjectTechFocus

---
+++ src/main/java/br/com/TechFocusAPI/TechFocusApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.TechFocusAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechFocusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechFocusApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/TechFocusAPI/TechFocusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.TechFocusAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechFocusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
